a03521732e8caa84c196d1686043b655edbda401
Gustavo Dias <gustavodias2@gmail.com>
2021-01-29 22:39:11 -0300

    Finalizando Aula 05

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import {motion} from 'framer-motion'
 
 import db from '../db.json';
 import Widget from '../src/components/Widget';
@@ -9,6 +10,7 @@ import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import Link from '../src/components/Link';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -39,7 +41,16 @@ export default function Home() {
       </Head>
       <QuizContainer>
         <QuizLogo />
-        <Widget>
+        <Widget 
+          as={motion.section}
+          transition={{delay:0, duration:0.5}}
+          variants={{
+            show: {opacity: 1, y: '0'},
+            hidden: {opacity: 0, y: '100%'},
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
@@ -67,18 +78,47 @@ export default function Home() {
           </Widget.Content>
         </Widget>
 
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{delay:0.5, duration:0.5}}
+          variants={{
+            show: {opacity: 1},
+            hidden: {opacity: 0},
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Content>
             <h1>Quizes da Galera</h1>
-              <a href='https://quiz-pokemon.vercel.app' target='_blank' >Quiz Pokemon</a>
-              <br></br>
-              <a href='https://quiz-dragon-ball-immersion-alura-next-js.vercel.app' target='_blank' >Quiz Dragon Ball</a>
-              <br></br>
-              <a href='https://animequiz.ffsf-filho.vercel.app/' target='_blank'>Quiz Animes</a>
-              <br></br>
+            <ul>
+              {db.external.map((linkExterno) => {
+                const [projectName,githubUser] = linkExterno
+                .replace(/\//g,'')
+                .replace('https:','')
+                .replace('vercel.app','')
+                .split('.');
+                return (
+                  <li key={linkExterno}>
+                    <Widget.Topic 
+                      as={Link}
+                      href="{linkExterno}" target="_blank">{`${githubUser}/${projectName}`}
+                    </Widget.Topic>
+                  </li>
+                  );
+              })}
+            </ul>
           </Widget.Content>
         </Widget>
-        <Footer />
+        <Footer 
+           as={motion.Footer}
+           transition={{delay:0.5, duration:0.5}}
+           variants={{
+             show: {opacity: 1},
+             hidden: {opacity: 0},
+           }}
+           initial="hidden"
+           animate="show"
+        />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/gpd38/cursoAluraImersaoReact2" />
 	</QuizBackground>
